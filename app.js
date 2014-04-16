dfe5e84634274bc761b9973cfad656dfe1a1c75d
Tom Horton <thomasphorton@gmail.com>
2014-04-16 19:18:34 -0400

    Handle RTs a little differently.

--- app.js
+++ app.js
@@ -8,15 +8,26 @@ var express = require('express'),
     Bot = require("./bot");
 
 var bot = new Bot({
-  consumer_key:         process.env.TWITTER_consumer_key,
-  consumer_secret:      process.env.TWITTER_consumer_secret,
-  access_token:         process.env.TWITTER_access_token,
-  access_token_secret:  process.env.TWITTER_access_token_secret
+  // consumer_key:         process.env.TWITTER_consumer_key,
+  // consumer_secret:      process.env.TWITTER_consumer_secret,
+  // access_token:         process.env.TWITTER_access_token,
+  // access_token_secret:  process.env.TWITTER_access_token_secret
+
+  consumer_key:'1jQxsWpkZoYYVMuJQXXLCw',
+  consumer_secret:'8INkXTGaPkcp0JzASRxHaMAY1QBCGFMLEvhut3Tng',
+  access_token:'2411690641-cvtxK39b9adeSYABMH682bBwii1cB4GeNCLwi2e',
+  access_token_secret:'iz1jKWhZKLBkfoy1iEbjKCWvK7RtQJxONsCvvoXwqzvPL'
 });
 
 bot.watch_mention(function(tweet) {
+
+  console.log('tweet');
   if (tweet.user.screen_name === 'tweetakeet') {
+    // No self tweets, please
     return false;
+  } else if (tweet.hasOwnProperty('retweeted_status')) {
+    // Retweets get truncated if they're too long, we need to treat them special
+    markov.add(tweet.retweeted_status.text, function() {});
   } else {
     markov.add(tweet.text, function() {
       if (tweet.text.indexOf('@tweetakeet') > -1) {
@@ -48,7 +59,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', pages.index);
-//app.get('/clear', markov.clear);
+app.get('/clear', markov.clear);
 app.get('/dictionary', pages.dictionary);
 app.get('/dictionary/:query', markov.dictionary);
 app.get('/generate', pages.generate);
